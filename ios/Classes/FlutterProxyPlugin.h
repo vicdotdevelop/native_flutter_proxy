9fefb7d40eca43761985bd28be817bdea89e0a99
Carlos Gutiérrez Moreno <carlosgutimo.3d@gmail.com>
2025-03-06 17:25:18 +0100

    Updates android config and documents code

--- ios/Classes/FlutterProxyPlugin.h
+++ ios/Classes/FlutterProxyPlugin.h
@@ -1,4 +1,11 @@
 #import <Flutter/Flutter.h>
 
-@interface FlutterProxyPlugin : NSObject<FlutterPlugin>
+/**
+ * @class FlutterProxyPlugin
+ * @brief A plugin class for handling proxy settings in Flutter.
+ *
+ * This class serves as an interface for the Flutter plugin, allowing
+ * the registration of the plugin with the Flutter engine.
+ */
+@interface FlutterProxyPlugin : NSObject <FlutterPlugin>
 @end
